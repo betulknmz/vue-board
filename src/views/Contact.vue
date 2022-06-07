7b9460d214dfb733ef24e0e9ad55dfd054b19003
betulknmz <betulyna0@gmail.com>
2022-06-08 00:35:03 +0300

    Contact sayfasına siteInfo eklendi

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,25 +1,34 @@
 <template>
-  <div>
+ <div>
+   <div class="max-w-sm mt-12 overflow-hidden bg-white rounded shadow-lg">
+        
+        <div class="px-6 py-4">
+          <div class="mb-2 text-xl font-bold text-gray-900">
+            {{siteInfo.siteName}}
+          </div>
+          <p class="text-base text-gray-700">
+            {{siteInfo.description}}
+          </p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          <span
+            class="inline-block px-3  mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full"
+            ><a>{{siteInfo.facebookUrl}}</a></span
+          >
+          <span
+            class="inline-block px-3  mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full"
+            ><a>{{siteInfo.instagramUrl}}</a></span
+          >
+          <span
+            class="inline-block px-3  mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full"
+            ><a>{{siteInfo.linkedinUrl}}</a></span
+          >
+        </div>
+      </div>
+
     <!-- Breadcrumb -->
     <!-- <Breadcrumb breadcrumb="Contact" /> -->
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">ID</th>
-          <th scope="col">Name</th>
-          <th scope="col">Image</th>
-          <th scope="col">Stock</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="product in products" :key="product.productId">
-          <th scope="row">{{ product.productId }}</th>
-          <td>{{ product.productName }}</td>
-          <td>{{ product.productImage }}</td>
-          <td>{{ product.stock }}</td>
-        </tr>
-      </tbody>
-    </table>
+    
   </div>
 </template>
 
@@ -28,20 +37,41 @@ import { ref, onMounted } from "vue";
 import axios from "axios";
 
 // import Breadcrumb from "../partials/Breadcrumb.vue";
+interface User {
+  name: string;
+  email: string;
+  title: string;
+  title2: string;
+  status: string;
+  role: string;
+}
 
-const products = ref([]);
+const testUser: User = {
+  name: "John Doe",
+  email: "john@example.com",
+  title: "Software Engineer",
+  title2: "Web dev",
+  status: "Active",
+  role: "Owner",
+};
+const siteInfo = ref([]);
 onMounted(async () => {
   await axios
-    .get("http://kozmosapi-001-site1.itempurl.com/api/Products")
+    .get("http://kozmosapi-001-site1.itempurl.com/api/SiteInfo")
     .then((response) => {
-      products.value = response.data;
+      siteInfo.value = response.data;
     });
-  console.log(products);
+  console.log(siteInfo);
 });
+// const deleteItem = async (productId) => {
+//   await axios
+//   .delete(`http://kozmosapi-001-site1.itempurl.com/api/Products/${productId}`)
+//   .then((response) => {
+//     let products = [...response.value]
+//   })
+// }
 
-//composition API
-
-const open = ref(false);
+const users = ref<User[]>([...Array(10).keys()].map(() => testUser));
 </script> 
 
 
